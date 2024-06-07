88904a5442096bd63d428ba712ae90eece617935
Vivek Mishra <vivek9142@gmail.com>
2024-06-08 02:26:11 +0530

    added admin dashboard and loading component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,104 @@
-export default function AdminDashboard(){
-    return <h1>Dashboard</h1>
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+
+import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+async function getUsersData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+      _count: true,
+    }),
+  ]);
+
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } }),
+  ]);
+
+  return {
+    activeCount,
+    inactiveCount,
+  };
+}
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+export default async function AdminDashboard() {
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUsersData(),
+    getProductData(),
+  ]);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <DashboardCard
+        title="Sales"
+        subtitle={`${formatCurrency(salesData.numberOfSales)} Orders`}
+        body={formatNumber(salesData.amount)}
+      />
+
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatNumber(userData.averageValuePerUser)} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(
+          productData.inactiveCount
+        )} Inactive Products`}
+        body={formatNumber(productData.activeCount)}
+      />
+    </div>
+  );
+}
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardDescription>{subtitle}</CardDescription>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
