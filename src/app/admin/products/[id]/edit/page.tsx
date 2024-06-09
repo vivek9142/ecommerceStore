5e9e528447140ad84e8de276103c856024808038
Vivek Mishra <vivek9142@gmail.com>
2024-06-10 00:03:20 +0530

    added edit product functionality

---
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,21 @@
+import PageHeader from "@/app/admin/_components/PageHeader";
+import ProductForm from "../../_components/ProductForm";
+import db from "@/db/db";
+
+export default async function EditProductsPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+  });
+  return (
+    <>
+      <PageHeader>Edit Product</PageHeader>
+      <ProductForm product ={product}/>
+    </>
+  );
+}
