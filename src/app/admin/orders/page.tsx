ddbf97872b35b7a57b653d371a7075ce02533dca
Vivek Mishra <vivek9142@gmail.com>
2024-07-16 05:44:04 +0530

    added Admin Orders viewing functionality

---
+++ src/app/admin/orders/page.tsx
@@ -0,0 +1,87 @@
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import PageHeader from "../_components/PageHeader";
+import db from "@/db/db";
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { DeleteDropdownItem } from "./_components/OrderActions";
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <PageHeader>Products</PageHeader>
+      <OrdersTable />
+    </>
+  );
+}
+
+function getOrders() {
+  return db.order.findMany({
+    select: {
+      id: true,
+      pricePaidInCents: true,
+      product: { select: { name: true } },
+      user: { select: { email: true } },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
+async function OrdersTable() {
+  const orders = await getOrders();
+  if (orders.length === 0) return <p>No Sales found.</p>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Product</TableHead>
+          <TableHead>Customer</TableHead>
+          <TableHead>Price Paid</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orders.map((order) => {
+          return (
+            <TableRow key={order.id}>
+              <TableCell>{order.product.name}</TableCell>
+              <TableCell>{order.user.email}</TableCell>
+              <TableCell>
+                {formatCurrency(order.pricePaidInCents / 100)}
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <MoreVertical />
+                    <span className="sr-only">Actions</span>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DeleteDropdownItem id={order.id} />
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+}
